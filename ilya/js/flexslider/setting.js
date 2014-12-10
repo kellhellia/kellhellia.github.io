34e9c835f76df921ea90171a4fc3ab3cb8e9be62
kellhellia <kellhellia@gmail.com>
2014-12-10 18:54:00 +0200

    add ilya website

---
+++ ilya/js/flexslider/setting.js
@@ -0,0 +1,16 @@
+(function($) {
+	'use strict';
+      $('.flexslider').flexslider({
+        animation: "slide",
+		directionNav: true,
+		controlNav: true,
+		slideshow: true, 
+      });
+	  
+      $('.imac-device').flexslider({
+        animation: "slide",
+		directionNav: false,
+		controlNav: false,
+		slideshow: true, 
+      });
+})(jQuery);
